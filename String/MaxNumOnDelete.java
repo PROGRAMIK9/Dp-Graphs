6da8db87d3571415078870d96968795d851e8bd0
M Aaryamann Prabhu <140898184+PROGRAMIK9@users.noreply.github.com>
2026-07-29 21:28:26 +0530

    Create MaxNumOnDelete.java

---
+++ String/MaxNumOnDelete.java
@@ -0,0 +1,23 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    private static int findMax(String num, char digit){
+        int n = num.length();
+        int max = 0;
+        for(int i = 0; i < n; i++){
+            if(num.charAt(i) == digit){
+                int val = Integer.parseInt(num.substring(0, i) + num.substring(i+1,n));
+                max = Math.max(max, val);
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number as a string");
+        String num = s.nextLine();
+        char dig = s.next().charAt(0);
+        System.out.println(findMax(num, dig));
+    }
+}
