a1d654da5d99f04630a1ad18d50551aad26221d4
M Aaryamann Prabhu <140898184+PROGRAMIK9@users.noreply.github.com>
2026-07-29 21:36:02 +0530

    Update MaxNumOnDelete.java

--- String/MaxNumOnDelete.java
+++ String/MaxNumOnDelete.java
@@ -4,14 +4,18 @@ import java.util.*;
 class Main {
     private static int findMax(String num, char digit){
         int n = num.length();
-        int max = 0;
+        int ind = -1;
         for(int i = 0; i < n; i++){
+            // if(num.charAt(i) == digit){
+            //     int val = Integer.parseInt(num.substring(0, i) + num.substring(i+1,n));
+            //     max = Math.max(max, val);
+            // }
             if(num.charAt(i) == digit){
-                int val = Integer.parseInt(num.substring(0, i) + num.substring(i+1,n));
-                max = Math.max(max, val);
+                ind =  i;
+                if(i < n-1 && num.charAt(i+1) > digit) break;
             }
         }
-        return max;
+        return Integer.parseInt(num.substring(0, ind)+num.substring(ind+1));
     }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
