6dc2ab469e26768bc56d4bb90f564b10b303bbba
M Aaryamann Prabhu <140898184+PROGRAMIK9@users.noreply.github.com>
2026-08-15 21:53:06 +0530

    ✅ 3702. Longest Subsequence With Non-Zero Bitwise XOR [Medium] — java

--- medium/3702-longest-subsequence-with-non-zero-bitwise-xor/solution.java
+++ medium/3702-longest-subsequence-with-non-zero-bitwise-xor/solution.java
@@ -4,7 +4,7 @@
                 if(xor>max_xor){
                     max_xor = xor;
                 }
-                    length = j-i+1;
+                    length = j-i;
             }
         }
     }
