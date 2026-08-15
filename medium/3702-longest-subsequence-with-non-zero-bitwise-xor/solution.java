5b6643a6680a34ce0ba56ad5f0aa08c49fb3fd16
M Aaryamann Prabhu <140898184+PROGRAMIK9@users.noreply.github.com>
2026-08-15 21:59:29 +0530

    ✅ 3702. Longest Subsequence With Non-Zero Bitwise XOR [Medium] — java

--- medium/3702-longest-subsequence-with-non-zero-bitwise-xor/solution.java
+++ medium/3702-longest-subsequence-with-non-zero-bitwise-xor/solution.java
@@ -1,13 +1,13 @@
-class Solution {
-    public int longestSubsequence(int[] nums) {
-        int val  = 0;
-        int n = nums.length;
-        boolean nonZero = false;
-        for(int num:nums){
-            nonZero |= num>0;
-            val ^= num;
-        }
-        if(!nonZero) return 0;
-        return val == 0? n-1 : n;
-    }
+class Solution {
+    public int longestSubsequence(int[] nums) {
+        int val  = 0;
+        int n = nums.length;
+        boolean nonZero = false;
+        for(int num:nums){
+            nonZero |= num>0;
+            val ^= num;
+        }
+        if(!nonZero) return 0;
+        return val == 0? n-1 : n;
+    }
 }
