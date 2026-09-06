999be3e42bf3eca88c19024bf92bc8666a4ba3e3
M Aaryamann Prabhu <140898184+PROGRAMIK9@users.noreply.github.com>
2026-09-06 13:01:21 +0530

    ✅ 38. Count and Say [Medium] — java

---
+++ medium/38-count-and-say/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n == 1) return "1";
+        String prev = countAndSay(n-1);
+        int currcnt = 1;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < prev.length()-1; i++){
+            while(i < prev.length()-1 && prev.charAt(i) == prev.charAt(i+1)){
+                currcnt++;
+                i++;
+                System.out.println(currcnt+" "+i);
+            }
+            sb.append(String.valueOf(currcnt) + prev.charAt(i));
+            currcnt = 1;
+        }
+        if(prev.length()<=1 || prev.charAt(prev.length()-1) != prev.charAt(prev.length()-2)) sb.append("1"+prev.charAt(prev.length()-1));
+        return sb.toString();
+    }
+}
