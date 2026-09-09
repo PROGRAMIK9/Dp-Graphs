d61c89beed356e706ada98f26cbdbd783a077660
M Aaryamann Prabhu <140898184+PROGRAMIK9@users.noreply.github.com>
2026-09-09 21:39:21 +0530

    Clarify logic in countCommas method
    Add a comment to clarify the logic for counting commas.

--- medium/3871-count-commas-in-range-ii/solution.java
+++ medium/3871-count-commas-in-range-ii/solution.java
@@ -2,6 +2,7 @@ class Solution {
     public long countCommas(long n) {
         if(n < 1000) return 0;
         long p = 1000, res = 0;
+        //checking for every 3rd group from the right
         while( p <= n){
             res += n - p +1;
             p *= 1000;
