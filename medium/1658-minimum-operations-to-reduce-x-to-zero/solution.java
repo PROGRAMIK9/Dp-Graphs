4c65181c43bd02bcc8606ac694217d504ab683df
M Aaryamann Prabhu <140898184+PROGRAMIK9@users.noreply.github.com>
2026-09-23 21:16:49 +0530

    ✅ 1658. Minimum Operations to Reduce X to Zero [Medium] — java

--- medium/1658-minimum-operations-to-reduce-x-to-zero/solution.java
+++ medium/1658-minimum-operations-to-reduce-x-to-zero/solution.java
@@ -2,20 +2,21 @@ class Solution {
     public int minOperations(int[] nums, int x) {
         int n = nums.length;
         int res = -x;
-        for(int num: nums){
+        for(int num : nums){
             res += num; 
-        }   
+        } 
+
         if( res < 0 ) return -1;
         if( res == 0 ) return n;
 
         int best = -1, i = 0, s = 0;
         for(int j = 0; j < n; j++){
             s += nums[j];
-            while( s > res){
-                s -= nums[i++];
+            while( s > res ){
+                s -= nums[ i++ ];
             }
-            if(s == res) best = Math.max(best, j - i + 1);
+            if(s == res) best = Math.max( best, j - i + 1 );
         }
-        return best < 0 ? -1 : n-best;
+        return best < 0 ? -1 : n - best;
     }
 }
